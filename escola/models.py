e68d41c2f49132ff5a074eb14f9b3ebb12600f64
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-02 14:38:37 -0300

    Cadastro de alunos

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Alunos (models.model):
+    nome = models.CharField (max_lenght=30)
+    rg = models.CharField (max_lenght=9)
+    cpf = models.CharField (max_lenght=11)
+    data_nascimento = models.DateField ()
+
+    def _str_ (self):
+        return self.nome 
+
+
+
 # Create your models here.
