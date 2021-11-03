2d06caab697b61c7073368daf44c39b2a4d4095c
Claudionice Rosah <rosahclaudionice@gmail.com>
2021-11-03 15:40:24 -0300

    Montando o Banco de Dados

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import alunos, curso
+
+class alunos (admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display = ('id', 'nome')
+    search_fields = ('nome')
+    list_per_page = 20
+
+    admin.site.register (alunos, alunos)
+
+    class curso (admin.ModelAdmin):
+        list_display = ('id', 'codigo_curso', 'descricao')
+        list_display_links = ('id', 'codigo_curso')
+        search_fields = ('codigo_curso')
+
+    admin.site.register (curso, curso)    
 
 # Register your models here.
